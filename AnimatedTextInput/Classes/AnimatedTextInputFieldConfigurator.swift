895f0844ca2e082c9fd1c703f5341eff76369e09
Raúl Uranga <ruranga@gmail.com>
2023-04-02 21:03:14 -0600

    update bundle reference

--- AnimatedTextInput/Classes/AnimatedTextInputFieldConfigurator.swift
+++ AnimatedTextInput/Classes/AnimatedTextInputFieldConfigurator.swift
@@ -70,7 +70,7 @@ fileprivate struct AnimatedTextInputPasswordConfigurator {
         if toggleable {
             let disclosureButton = UIButton(type: .custom)
             disclosureButton.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 20, height: 20))
-            let bundle = Bundle(path: Bundle(for: AnimatedTextInput.self).path(forResource: "AnimatedTextInput", ofType: "bundle")!)
+            let bundle = Bundle(for: AnimatedTextInput.self)
             let normalImage = UIImage(named: "cm_icon_input_eye_normal", in: bundle, compatibleWith: nil)
             let selectedImage = UIImage(named: "cm_icon_input_eye_selected", in: bundle, compatibleWith: nil)
             disclosureButton.setImage(normalImage, for: .normal)
@@ -112,7 +112,7 @@ fileprivate struct AnimatedTextInputSelectionConfigurator {
     
     static func generate() -> TextInput {
         let textField = AnimatedTextField()
-        let bundle = Bundle(path: Bundle(for: AnimatedTextInput.self).path(forResource: "AnimatedTextInput", ofType: "bundle")!)
+        let bundle = Bundle(for: AnimatedTextInput.self)
         let arrowImageView = UIImageView(image: UIImage(named: "disclosure", in: bundle, compatibleWith: nil))
         textField.rightView = arrowImageView
         textField.rightViewMode = .always
